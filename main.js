a4c6885c4dcc1e003a787a1f487086d47182db4b
Rodrigo Azevedo Silva <rodrigo.azvd750@gmail.com>
2023-05-27 23:06:30 -0300

    commit

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-  <h2>Olá, Mundo!</h2>
+  <h2>Olá, Vite!</h2>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
